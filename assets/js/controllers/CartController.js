6a4a8834c83b74f32e6ab7e90e04ba823e44c7c7
Basma Essam <Basma.Essam@STA.com>
2019-07-24 16:58:15 +0200

    CartPage

---
+++ assets/js/controllers/CartController.js
@@ -0,0 +1,47 @@
+OnlineShoppingApp.controller('CartController',
+    function CartController($scope,CartService,localStorageService,ProductService,$location,$window){
+       $scope.carts;
+       $scope.userId=localStorageService.Get("UserId");
+       CartService.GetUserCarts($scope.userId).then(function(response){
+
+           $scope.carts=response.data;
+          // console.log($scope.carts)
+           $scope.carts.forEach(cart => {
+           ProductService.GetProduct(cart.productId).then(function(response){
+               cart.product=response.data;
+               if(cart.product.quantity>=cart.quantity)
+               {
+                   cart.InStock="InStock";
+               }
+               else
+               cart.InStock="OutStock";
+           })   
+              ProductService.GetProductImages(cart.productId).then(function(response){
+                  cart.image=response.data[0].imageUrl;
+              }) 
+           });
+       }).catch(function(error){
+        console.log("noo");
+
+       })
+
+       $scope.Delete = function(cart)
+       {
+           CartService.DeleteCart(cart).then(function(response){
+               //console.log("donee",response);
+               $window.alert("item removed successfully");
+               $window.location.reload();
+              // $location.path('/cart');               
+           }).catch(function(error){
+               console.log(error);
+           })
+       }
+       $scope.SubmitOrder= function(){
+           console.log("hii",$scope.userId);
+           CartService.SubmitOrder($scope.userId).then(function(response){
+               $window.alert("order submitted successfuly");
+               $window.location.reload();
+           })
+       }
+
+    });
